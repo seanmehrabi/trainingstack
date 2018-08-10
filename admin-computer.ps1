4c2672f70ce8fecbb889c6a982788f4ad52697b7
seanmehrabi <sean@podiumcatchers.com>
2018-08-10 10:20:53 -0700

    Updating admin tools
    Installing Admin tools using Choco

--- admin-computer.ps1
+++ admin-computer.ps1
@@ -1,23 +1,17 @@
-mkdir c:\temp\install\
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
-
-$webClient = New-Object –TypeName System.Net.WebClient
-
-$webClient.DownloadFile('https://github.com/git-for-windows/git/releases/download/v2.17.1.windows.2/Git-2.17.1.2-64-bit.exe','c:\temp\install\git-setup.exe')
-
-$webClient.DownloadFile('https://notepad-plus-plus.org/repository/7.x/7.5.6/npp.7.5.6.Installer.x64.exe','c:\temp\install\npp-setup.exe')
-
-$webClient.DownloadFile('https://az764295.vo.msecnd.net/stable/6a6e02cef0f2122ee1469765b704faf5d0e0d859/VSCodeSetup-x64-1.24.0.exe','c:\temp\install\vc-setup.exe')
-
-
-$pathvargs = {c:\temp\install\git-setup.exe /verysilent /S /v/qn }
-Invoke-Command -ScriptBlock $pathvargs
-
-$pathvargs = {c:\temp\install\npp-setup.exe  /S /v/qn }
-Invoke-Command -ScriptBlock $pathvargs
-
-$pathvargs = {c:\temp\install\vc-setup.exe /verysilent /S /v/qn }
-Invoke-Command -ScriptBlock $pathvargs
-
-Install-Module -Name AWSPowerShell
+#installing DevOps Admin Tools in Windows using Choco
+$script = New-Object Net.WebClient
+$script.DownloadString("https://chocolatey.org/install.ps1")
+Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+choco upgrade chocolatey
+choco install -y nano
+choco install -y python
+choco install -y notepadplusplus
+choco install -y git.install
+choco install -y googlechrome
+choco install -y firefox
+choco install -y awscli
+choco install -y awstools.powershell
+choco install -y curl
+choco install -y atom
+choco install -y putty
+choco install -y visualstudiocode
