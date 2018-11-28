d73c2ca8c880a76ddd756b0ef8c1b432d95cd6a5
Sean Mehrabi <sean@podiumcatchers.com>
2018-11-28 14:28:54 -0800

    Added Admin Computer setup for MacOS
    Added Support for MacOS

---
+++ admin-computer-macos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update                           # Fetch latest version of homebrew and formula.
+brew cask install firefox
+brew cask install google-chrome 
+brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
+brew cask install atom 
+brew cask install sublime-text 
+brew install python
+brew cask install minikube
+brew install kubernetes-cli 
+brew install azure-cli
+brew install awscli
+sudo easy_install pip
+brew install jq 
+brew install yq 
+brew cask install powershell
+brew install git 
+brew install curl
+brew install putty 
+brew cask install chef/chef/chefdk
+brew install ansible
+brew install terraform
+brew cask install visual-studio-code
