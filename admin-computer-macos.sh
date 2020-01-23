f9a7a2444876f9ddfc94a0ea22beb0c94e7a5a54
Sean Mehrabi <sean@podiumcatchers.com>
2020-01-22 20:53:29 -0800

    Update admin-computer-macos.sh

--- admin-computer-macos.sh
+++ admin-computer-macos.sh
@@ -5,13 +5,10 @@ brew update                           # Fetch latest version of homebrew and for
 brew cask install firefox
 brew cask install google-chrome 
 brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
+brew install kubernetes-cli 
 brew cask install atom 
 brew cask install sublime-text 
 brew install python
-brew cask install minikube
-brew install kubernetes-cli 
-brew install azure-cli
-brew install awscli
 sudo easy_install pip
 brew install jq 
 brew install yq 
@@ -19,9 +16,23 @@ brew cask install powershell
 brew install git 
 brew install curl
 brew install putty 
+brew install bash-completion
 brew cask install chef/chef/chefdk
 brew install ansible
 brew install terraform
 brew cask install visual-studio-code
 #install some cool software 
 brew cask install spectacle
+
+#AWS 
+brew install awscli
+brew install aws-iam-authenticator
+
+#Azure 
+brew install azure-cli
+
+#Google 
+brew cask install google-cloud-sdk 
+
+#AliCloud
+brew install aliyun-cli
