536542d95ccf5137baa8f191e20f9f98b380e9e0
Sean Mehrabi <sean@podiumcatchers.com>
2021-01-21 23:34:01 -0800

    Update admin-computer-macos.sh

--- admin-computer-macos.sh
+++ admin-computer-macos.sh
@@ -2,27 +2,27 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update                           # Fetch latest version of homebrew and formula.
-brew cask install firefox
-brew cask install google-chrome 
+brew install --cask firefox
+brew install --cask google-chrome 
 brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
 brew install kubernetes-cli 
-brew cask install atom 
-brew cask install sublime-text 
+brew install --cask atom 
+brew install --cask sublime-text 
 brew install python
 sudo easy_install pip
 brew install jq 
 brew install yq 
-brew cask install powershell
+brew install --cask powershell
 brew install git 
 brew install curl
 brew install putty 
 brew install bash-completion
-brew cask install chef/chef/chefdk
+brew install --cask chef/chef/chefdk
 brew install ansible
 brew install terraform
-brew cask install visual-studio-code
+brew install --cask visual-studio-code
 #install some cool software 
-brew cask install spectacle
+brew install --cask spectacle
 
 #AWS 
 brew install awscli
@@ -32,7 +32,7 @@ brew install aws-iam-authenticator
 brew install azure-cli
 
 #Google 
-brew cask install google-cloud-sdk 
+brew install --cask google-cloud-sdk 
 
 #AliCloud
 brew install aliyun-cli
